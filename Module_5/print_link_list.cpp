500622cc1074231a8c0e1ef551f6c59c81d638fe
subahan ali <subahanislam523@gmail.com>
2025-02-05 13:42:20 +0600

    print link list

--- Module_5/print_link_list.cpp
+++ Module_5/print_link_list.cpp
@@ -13,14 +13,22 @@ public:
 };
 int main()
 {
-
     Node *head = new Node(10);
     Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
 
     head->next = a;
-
-    cout << head->val << endl;
-    cout << a->val << endl;
+    a->next = b;
+    b->next = c;
+    c->next = d;
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << endl;
+        tmp = tmp->next;
+    }
 
     return 0;
 }
